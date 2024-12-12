4d4a1c8adc1a0aab7e0f491175364c4e93e0d1f2
MVDeguzman <markvincentdg06@gmail.com>
2024-12-12 12:26:50 +0800

    Add files via upload

--- activity22.py
+++ activity22.py
@@ -1,25 +1,30 @@
-# def act1():
-#     print("hello world")
-# def act2():
-#     pname= input("please enter your name:")
-# print("Hi" + name)
-# def act4():
-#     number1= input("enter a numbe:")
-#     number2= input("enter a number:")
-#     answer= number1 + number2
-#     print("The sum of:",number1 + " + " +number2 + " is " + answer)
-# def act5():
+def act1():
+    print("hello world")
+def act2():
+    pname= input("please enter your name:")
+print("Hi" + name)
+def act4():
+    number1= input("enter a numbe:")
+    number2= input("enter a number:")
+    answer= number1 + number2
+    print("The sum of:",number1 + " + " +number2 + " is " + answer)
+def act5():
+    print("FARENHEIT TO CELSIUS CONVERTER PROGRAM")
+farenheit= eval(input("enter temperature in farenheit:"))
+celsius= ((farenheit-32)* 5 )/9
 
-# def act6():
-#     x = 5
+print(f"{farenheit}, degrees Farenheit converted to celsius is {round(celsius,2)} degrees")
 
-#     rint(x)
+def act6():
+    x = 5
 
-#     x =  x + 10
+    print(x)
 
-#     print(x)
+    x =  x + 10
 
-#     x =  x + 15
+    print(x)
 
-#     print(x)
+    x =  x + 15
+
+    print(x)
 
